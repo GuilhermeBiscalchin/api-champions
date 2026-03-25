3a84c7205b515eea7a9c3c85814697601af14c47
Guilherme <representacoesgap@gmail.com>
2026-03-24 21:47:10 -0300

    instalacao express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+import express, { json, Request, Response } from "express";
+
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use(json())
+
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).json({player: "Ronaldo"});
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
