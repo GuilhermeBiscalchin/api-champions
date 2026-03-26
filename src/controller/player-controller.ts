a26157d9e935a0389d9a1f37edd6091a0de2d533
Guilherme <representacoesgap@gmail.com>
2026-03-25 21:29:15 -0300

    criacao do model

--- src/controller/player-controller.ts
+++ src/controller/player-controller.ts
@@ -1,10 +1,8 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 import { getPlayerService } from "../service/player-service";
-
 
 export const getPlayer = async (request: Request, response: Response) => {
   const httpResponse = await getPlayerService();
- 
 
   response.status(httpResponse.statusCode).json(httpResponse.body);
 };
--- src/repositories/player-repositories.ts
+++ src/repositories/player-repositories.ts
@@ -1,7 +1,5 @@
-interface PlayerModel {
-  id: number;
-  name: string;
-}
+import { PlayerModel } from "../model/Player-Model";
+
 
 const database: PlayerModel[] = [
   { id: 1, name: "Messi" },
